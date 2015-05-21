d310047162465a4d2cd7cdc1c56b2b20d85548ef
lxw0109 <lxw0109@gmail.com>
2015-05-21 23:58:24 +0800

    num5 dynamic programming

---
+++ LeetCode_CPP/num005/num005_dp.cpp
@@ -0,0 +1,70 @@
+// File: num005_dp.cpp
+// Author: lxw
+// Date: 2015-05-20
+
+/*
+Num 005: Longest Palindromic Substring
+Source: https://leetcode.com/problems/longest-palindromic-substring/
+
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+#include <iostream>
+
+class Solution {
+    public:
+        std::string longestPalindrome(std::string s) {
+            std::string result = "";
+            int length = s.length();
+            int** array2 = new int* [length];
+            int maxLength = 1;
+            //std::string maxStr = std::string(s[0]);
+            std::string maxStr = "";
+            maxStr.insert(maxStr.begin(), s[0]);
+            for(int i = 0; i < length; ++i){
+                array2[i] = new int[length];
+            }
+
+            //length == 1
+            //Is this operation essential? Yes. Note the calculation of the "blue line" in the picture.
+            for(int i = 0; i < length; ++i){
+                array2[i][i] = true;
+            }
+            //length == 2
+            for(int i = 0; i < length-1; ++i){
+                if(s[i] == s[i+1]){
+                    array2[i][i+1] = true;
+                    maxLength = 2;
+                    maxStr = s.substr(i, 2);
+                    //std::cout << maxStr << std::endl;
+                }
+                else{
+                    array2[i][i+1] = false;
+                }
+            }
+            //length >= 3
+            for(int curLength = 3; curLength <= length; ++curLength){
+                for(int i = 0; i < length-curLength+1; ++i){
+                    int j = i + curLength - 1;
+                    if(array2[i+1][j-1] && s[i] == s[j]){
+                        array2[i][j] = true;
+                        maxLength = curLength;
+                        maxStr = s.substr(i, maxLength);
+                        //std::cout << "array2[" << i+1 << "][" << j-1 << "]:" << array2[i+1][j-1] << "\ts[" << i << "]:" << s[i] << ", s[" << j << "]:" << s[j] << std::endl;
+                        //std::cout << maxStr << std::endl;
+                    }
+                    else{
+                        array2[i][j] = false;
+                        //std::cout << "array2[" << i+1 << "][" << j-1 << "]:" << array2[i+1][j-1] << "\ts[" << i << "]:" << s[i] << ", s[" << j << "]:" << s[j] << std::endl;
+                    }
+                    //}
+                }
+            }
+            return maxStr;
+        }
+};
+
+int main(int argc, char ** argv){
+	Solution s;
+	std::cout << s.longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") << std::endl;
+	return 0;
+}
---
+++ LeetCode_CPP/num005/num005_string.cpp
@@ -0,0 +1,42 @@
+// File: num005.cpp
+// Author: lxw
+// Date: 2015-05-20
+
+/*
+Num 005: Longest Palindromic Substring
+Source: https://leetcode.com/problems/longest-palindromic-substring/
+
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+class Solution {
+    public:
+        string getPalindrome(string s, int left, int right){
+            int length = s.length();
+            while(left >= 0 && right < length){
+                if(s[left] != s[right]){
+                    break;
+                }
+                --left;
+                ++right;
+            }
+            return s.substr(left+1, right-left-1);
+        }
+        string longestPalindrome(string s) {
+            int length = s.length();
+            string longestStr = s.substr(0, 1);
+            for(int i = 0; i < length-1; ++i){
+                string retStr = getPalindrome(s, i, i);
+                if(retStr.length() > longestStr.length()){
+                    longestStr = retStr;
+                }
+                if(s[i] == s[i+1]){
+                    retStr = getPalindrome(s, i, i+1);
+                    if(retStr.length() > longestStr.length()){
+                        longestStr = retStr;
+                    }
+                }
+            }
+            return longestStr;
+        }
+};
