73eb1eba0691b030c597c4658206cea49de34326
lxw0109 <lxw0109@gmail.com>
2015-06-17 23:57:36 +0800

    update toString.cpp

--- template/toString.cpp
+++ template/toString.cpp
@@ -16,7 +16,9 @@ char *c = aa.c_str();
 
 #include <iostream>
 #include <sstream> //std::stringstream, std::stringbuf
+#include <string>
 #include <cstdlib>
+#include <algorithm> //std::reverse
 using namespace std;
 
 class intCharStar{
@@ -26,9 +28,9 @@ private:
 public:
 	intCharStar(int number, char * ptr):number(number), pointer(ptr){}
 
+	//char* -> int
 	int charStarToInt(){
-		/*
-		char* -> int
+		/*		
 		#include <cstdlib>
 		int atoi(const char *nptr);
 		long atol(const char *nptr);
@@ -38,64 +40,121 @@ public:
 		return atoi(this->pointer);
 	}
 
+	//int -> char *
 	char * intToCharStar(){
 		/*
-		int -> char *
 		itoa(i,num,10);
 		i 需要转换成字符的数字
 		num 转换后保存字符的变量
 		10 转换数字的基数（进制）10就是说按照10进制转换数字。还可以是2，8，16等等你喜欢的进制类型
-		原形：char *itoa(int value, char* string, int radix);
-		*/
-
-		/*
+		
+		Demo:
 		int i=1234;
 		char s[5];
 		itoa(i,s,10);
 		printf("%s",s);
 		*/
-		itoa(this->number, this->pointer, 10);
-		return this->pointer;
+		return itoa(this->number, this->pointer, 10);
 	}
-};
 
-class ToString{
-public:
-    //char * -> string
-    string toString(const char * pointer){
-        string result = pointer;
-        return result;
-    }
+	//int -> string
+	// std::string style "itoa":
+	std::string itoa(int value, int base) {
+		std::string buf;
 
-    //char -> string
-    string toString(char ch){
-    	/*
-        stringstream ss;
-        ss << ch;
-        string result = ss.str();*/
-        string result = ch;
-        return result;
-    }
-    //int -> string
-    string toString(int number){
-        /*
-        stringstream ss;
-        ss << number;
-        string result = ss.str();
-        */
-        string result = number + '0';
-        return result;
-    }
+		// check that the base if valid
+		if (base < 2 || base > 16) return buf;
+
+		enum { kMaxDigits = 35 };
+		buf.reserve( kMaxDigits ); // Pre-allocate enough space.
+
+		int quotient = value;
+
+		// Translating number to string with base:
+		do {
+			buf += "0123456789abcdef"[ std::abs( quotient % base ) ];
+			quotient /= base;
+		} while ( quotient );
+
+		// Append the negative sign
+		if ( value < 0)
+			buf += '-';
+
+		std::reverse(buf.begin(), buf.end());
+		return buf;
+	}
+
+	//int -> char *
+	//char* style "itoa":
+	char* itoa(int value, char* result, int base) {
+		// check that the base if valid
+		if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+		char* ptr = result, *ptr1 = result, tmp_char;
+		int tmp_value;
+
+		do {
+			tmp_value = value;
+			value /= base;
+			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+		} while ( value );
+
+		// Apply negative sign
+		if (tmp_value < 0) *ptr++ = '-';
+		*ptr-- = '\0';
+		while(ptr1 < ptr) {
+			tmp_char = *ptr;
+			*ptr--= *ptr1;
+			*ptr1++ = tmp_char;
+		}
+		return result;
+	}
 };
 
 int main(void){
     ToString ts;
-    char * helloStr;
-    helloStr = "hello world!";
+    char * helloStr = "hello world!";
     cout << ts.toString(helloStr) << endl;
+
     char ch = 'a';
     cout << ts.toString(ch) << endl;
+
     int number = 123;
     cout << ts.toString(number) << endl;
+
+
+    string str;
+	char * ptr = "hello";
+    //char * -> string
+    str = ptr;  // ????
+    cout << "str: " << str << endl;
+
+    //char -> string
+    char ch = 'a';
+    stringstream ss;
+    ss << ch;
+    str = ss.str();
+    cout << "str: " << str << endl;
+
+    //int -> string
+    int number = 123;
+    stringstream ss;
+    ss << number;
+    str = ss.str();
+    cout << "str: " << str << endl;
+
+    //string -> const char *    
+    //只能转换成const char*，如果去掉const编译不能通过
+    const char * constStr = str.c_str();
+    const char * constStr1 = str.data();
+    cout << "constStr: " << cStr << endl;
+    cout << "constStr1: " << cStr1 << endl;
+
+    //string -> char *
+    int length = str.length();
+    char * chStr = new char[length + 1];
+    str.copy(chStr, length, 0);
+    delete[] chStr;
+    cout << "str: " << str << endl;
     return 0;
 }
