5b702c3f3f9d72711f2a4637af3b1fe7d55abd72
lxw0109 <lxw0109@gmail.com>
2015-06-28 15:09:20 +0800

    num054 OK

--- LeetCode_CPP/num054/num054.cpp
+++ LeetCode_CPP/num054/num054.cpp
@@ -1,3 +1,24 @@
+// File: num054.cpp
+// Author: lxw
+// Date: 2015-06-28
+
+/*
+Num num054: Spiral Matrix 
+Source: https://leetcode.com/problems/spiral-matrix/
+
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+For example,
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
