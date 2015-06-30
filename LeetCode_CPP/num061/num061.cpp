5c4eb6b385d72a2f4efc4e0580c7d6c3d8c7ca99
lxw0109 <lxw0109@gmail.com>
2015-06-30 13:00:29 +0800

    add num061

--- LeetCode_CPP/num061/num061.cpp
+++ LeetCode_CPP/num061/num061.cpp
@@ -21,6 +21,35 @@ return 4->5->1->2->3->NULL.
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+//There is no trick for this problem. Some people used slow/fast pointers to find the tail node,
+//but I don't see the benefit (in the sense that it doesn't reduce the pointer move op) to do so.
+//So I just used one loop to find the length first.
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL)
+            return head;
+
+        int len = 1; // number of nodes
+        ListNode *newH, *tail;
+        newH = tail = head;
+
+        while(tail->next){  // get the number of nodes in the list
+            tail = tail->next;
+            len++;
+        }
+        tail->next = head; // circle the link
+
+        if(k %= len){
+            for(auto i=0; i<len-k; i++) tail = tail->next; // the tail node is the (len-k)-th node (1st node is head)
+        }
+        newH = tail->next; 
+        tail->next = NULL;
+        return newH;
+    }
+};
+/*
 class Solution{
 public:
     ListNode* rotateRight(ListNode* head, int k) {
@@ -61,3 +90,4 @@ public:
         return head;
     }
 };
+*/
