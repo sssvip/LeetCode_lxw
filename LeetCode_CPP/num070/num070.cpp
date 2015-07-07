d4b736017bbda4b860af4652197f1579e02f48d0
lxw0109 <lxw0109@gmail.com>
2015-07-07 23:11:40 +0800

    update num070

--- LeetCode_CPP/num070/num070.cpp
+++ LeetCode_CPP/num070/num070.cpp
@@ -10,7 +10,9 @@ You are climbing a stair case. It takes n steps to reach to the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
-
+//DP
+//climbStairs(n) = climbStairs(n-1) + climbStairs(n-2);
+//climbStairs(n) = n (if n < 3)
 class Solution {
 public:
     int climbStairs(int n) {
