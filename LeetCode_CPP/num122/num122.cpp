5a74bf998cd51fd220f4288a027d6201545b6b99
lxw0109 <lxw0109@gmail.com>
2015-07-13 22:34:08 +0800

    update num122

--- LeetCode_CPP/num122/num122.cpp
+++ LeetCode_CPP/num122/num122.cpp
@@ -11,10 +11,9 @@ Say you have an array for which the ith element is the price of a given stock on
 Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 */
 
-//所有的单调递增区间
-
 class Solution {
 public:
+    //所有的单调递增区间.
     int maxProfit(vector<int>& prices) {
         int length = prices.size();
         if(length < 2)
@@ -35,6 +34,22 @@ public:
                 }
             }
             profit += prices[i] - prices[start];
+            ++i;
+        }
+        return profit;
+    }
+    //既然交易次数没有限定，可以看出只要对于每次两天差价大于0的都进行交易，就可以得到最大利润。因此算法其实就是累加所有大于0的差价既可以了.
+    int maxProfit(vector<int>& prices) {
+        int length = prices.size();
+        if(length < 2)
+            return 0;
+        int i = 0;
+        int profit = 0;
+        int delta;
+        while(i < length-1){
+            delta = prices[i+1] - prices[i];
+            if(delta > 0)
+                profit += delta;
             ++i;
         }
         return profit;
