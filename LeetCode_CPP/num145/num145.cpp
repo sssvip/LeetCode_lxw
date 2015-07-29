b026ae0eb2d14f76cc532fde3f6ae0750d4d6d1a
lxw0109 <lxw0109@gmail.com>
2015-07-29 16:29:07 +0800

    num145 OK

--- LeetCode_CPP/num145/num145.cpp
+++ LeetCode_CPP/num145/num145.cpp
@@ -48,3 +48,34 @@ public:
 };
 
 //Iterative
+struct Tuple{
+    Tuple(TreeNode * tn, int value):node(tn), tag(value){}
+    TreeNode * node;
+    int tag;
+};
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postOrder;
+        stack<Tuple> s;
+        if(root == NULL)
+            return postOrder;
+        s.push(Tuple(root, 0));
+        while(!s.empty()){
+            Tuple & t = s.top();
+            s.pop();
+            if(t.tag == 0){ //first time pop out.
+                //入栈的顺序非常重要: 先入当前节点, 然后入右节点, 最后入左节点
+                s.push(Tuple(t.node, 1));
+                if(t.node->right)
+                    s.push(Tuple(t.node->right, 0));
+                if(t.node->left)
+                    s.push(Tuple(t.node->left, 0));
+            }
+            else{   //second time pop out.
+                postOrder.push_back(t.node->val);
+            }
+        }
+        return postOrder;
+    }
+};
