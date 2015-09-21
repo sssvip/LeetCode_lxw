35f04ce219ff5b42c723789bc34f55411c5cc6d5
lxw0109 <lxw0109@gmail.com>
2015-09-21 14:38:05 +0800

    update num083

--- LeetCode_CPP/num082/num082.cpp
+++ LeetCode_CPP/num082/num082.cpp
@@ -39,57 +39,66 @@ void showList(ListNode * head){
     cout << head->val << endl;
 }
 
+
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head == NULL)
-            return NULL;
-        ListNode ln = ListNode(0);
-        ListNode * newHead = &ln;
-        ListNode * current = newHead;
-        int value = head->val;
-        int count = 0;
-        while(head){            
-            if(head->val == value){
+            return head;
+        ListNode * prev = new ListNode(0);
+        ListNode * sentinel = prev;
+        prev->next = head;
+        ListNode * cur = head;
+        ListNode * temp;
+        int count;
+        while(cur){
+            count = 1;
+            while(cur->next != NULL && cur->next->val == cur->val){
                 ++count;
+                temp = cur;
+                cur = cur->next;
+                delete temp;
+            }
+            if(count > 1){
+                temp = cur;
+                cur = cur->next;
+                delete temp;
             }
             else{
-                if(count == 1){                 
-                    /*
-                    //NOT OK. when came in the second time, ln1 will be modified instead of being recreated.
-                    ListNode ln1 = ListNode(value);
-                    current->next = &ln1;
-                    */
-                    /*NOT OK.
-                    ListNode ln1(value);
-                    current->next = &ln1;
-                    */
-                    current->next = new ListNode(value);
-                    current = current->next;
-                }
-                count = 1;
-                value = head->val;
+                prev->next = cur;
+                prev = cur;
+                cur = cur->next;
             }
-            head = head->next;            
         }
-        if(count == 1){
-            /*
-            ln = ListNode(value);   //NOTE: this is a 'trap and pitfall'. "ln = ListNode(value);" WON'T create a new ListNode, but modify the old ln in line 6;
-            current->next = &ln;
-            */
-            current->next = new ListNode(value);            
-        }
-        return newHead->next;
+        prev->next = NULL;
+        head = sentinel->next;
+        delete sentinel;
+        return head;
     }
 };
+
 int main(void){
 	Solution sol;
 	//LinkedList
-    ListNode ln1 = ListNode(1);
-    ListNode ln2 = ListNode(2);
-    ln1.next = &ln2;
-    //showList(&ln1);
-	ListNode * head = sol.deleteDuplicates(&ln1);
+    ListNode * ln1 = new ListNode(1);
+    ListNode * ln2 = new ListNode(2);    
+    ListNode * ln3 = new ListNode(3);
+    ListNode * ln4 = new ListNode(3);
+    ListNode * ln5 = new ListNode(4);
+    ListNode * ln6 = new ListNode(5);
+    ln1->next = ln2;
+    ln2->next = ln3;
+    ln3->next = ln4;
+    ln4->next = ln5;
+    ln5->next = ln6;
+    //showList(ln1);
+	ListNode * head = sol.deleteDuplicates(ln1);
 	showList(head);
+	ListNode * temp;
+	while(head){
+		temp = head;
+		head = head->next;
+		delete temp;
+	}
     return 0;
 }
