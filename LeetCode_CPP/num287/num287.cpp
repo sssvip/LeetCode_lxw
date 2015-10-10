c23bf63ff25b9a0fa875350c424508b5e73e2e44
lxw0109 <lxw0109@gmail.com>
2015-10-10 11:24:49 +0800

    update num287

--- LeetCode_CPP/num287/num287.cpp
+++ LeetCode_CPP/num287/num287.cpp
@@ -17,6 +17,40 @@ Credits:
 Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 */
 
+//Method 2:
+//二分查找, 1..10, 小于等于5(1+(10-1)/2)的一定有5个,如果多于5个,就在lower part, 等于5个就是upper part. 
+class Solution {
+private:
+    int getCount(vector<int>& nums, int target, int length){
+        int count = 0;
+        for(int i = 0; i < length; ++i){
+            if(nums[i] <= target)
+                ++count;
+        }
+        return count;
+    }
+public:
+    int findDuplicate(vector<int>& nums) {
+        int length = nums.size();
+        if(length < 3)
+            return 1;
+        int low = 1;
+        int high = length - 1;
+        int mid = 0;
+        while(low < high){
+            mid = low + ((high - low) >> 1);
+            if(getCount(nums, mid, length) > mid)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
+
+/*
+//Method 1:
+//想出来了这种方法,但不能证明是对的!
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -36,3 +70,4 @@ public:
         }
     }
 };
+*/
