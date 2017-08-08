02616e09899f27f4ccfda06d2271756c8a21044d
Xiaowei Liu <liu_xiaowei@foxmail.com>
2017-08-08 23:11:26 +0800

    Update num001.py

--- LeetCode_Python/num001/num001.py
+++ LeetCode_Python/num001/num001.py
@@ -37,8 +37,8 @@ class Solution:
                 break
         return index0, index1
     '''
-    # A really SIMPLE & CLEAN solution.
-    def twoSum1(self, num, target):
+    # OK: A really SIMPLE & CLEAN solution.
+    def twoSum(self, num, target):
         s = {}
         for i, element in enumerate(num):
             i2 = s.get(target-element)
@@ -47,47 +47,12 @@ class Solution:
             else:
                 s[element] = i
 
-    def twoSum(self, num, target):
-        length = len(num)
-        dic = {}
-        for i in xrange(length):
-            # dic[num[i]] = i
-            try:
-                var = dic[num[i]]
-            except KeyError:
-                dic[num[i]] = [i]
-            else:
-                dic[num[i]].append(i)
-
-        index0 = -1
-        flag = False
-        for key in xrange(length):
-            if flag:
-                break
-            other = target - num[key]
-            index1 = dic.get(other)  # return a "list" instead of an "int".
-
-            if index1:
-                length = len(index1)
-                for i in xrange(length):
-                    if index1[i] > key:
-                        index0 = key + 1
-                        index1[i] += 1
-                        flag = True
-                        break
-
-        return index0, index1[i]
-
-
 def main():
     sol = Solution()
     num = []
     for i in xrange(1000):
         num.append(i)
     num = [0, 4, 3, 4, 0, 0, 5]
-    # print sol.twoSum(num, 1900)
-    # It's possible and right when the following 2 methods show different answers.
-    print sol.twoSum(num, 9)
-    print sol.twoSum1(num, 9)
-
+    print(sol.twoSum(num, 9))
+    
 main()
